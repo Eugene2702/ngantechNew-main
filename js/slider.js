09f47c33005856b8291e73c3c88c34dd664e56dc
gleb <example@gmail.com>
2023-05-11 23:26:57 +0300

    first commit

---
+++ js/slider.js
@@ -0,0 +1,11 @@
+new Splide('#image-carousel', {
+    direction: 'ttb',
+    height: '200px'
+}).mount();
+
+
+new Splide('#service-carousel', {
+    type: 'loop',
+    height: '370px',
+    width: '200px',
+}).mount();
